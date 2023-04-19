0885c5a1524bd907555d6dce7957aed0da934f27
Abdurrahim Surtee <113867655+Surtee97@users.noreply.github.com>
2023-04-19 17:26:10 -0500

    fixed indentation

--- server.py
+++ server.py
@@ -18,40 +18,40 @@ serverSocket = socket(AF_INET, SOCK_DGRAM)
 serverSocket.bind(('', serverPort))
 print ("The server is ready to receive")
 while True:
-clientID, clientAddress = serverSocket.recvfrom(2048)
-checkID = clientID.decode()
-print(checkID)
-#if checkID != k_a:
-   # message = "Key does not match"
-   # serverSocket.sendto(message.encode(), clientAddress)
+   clientID, clientAddress = serverSocket.recvfrom(2048)
+   checkID = clientID.decode()
+   print(checkID)
+   #if checkID != k_a:
+      # message = "Key does not match"
+      # serverSocket.sendto(message.encode(), clientAddress)
 
-rand = random.randint(1,10)
-h = hashlib.new('sha256')
-h2 = hashlib.new('md5')
+   rand = random.randint(1,10)
+   h = hashlib.new('sha256')
+   h2 = hashlib.new('md5')
 
-hashString = str(rand)+k_a
-hashFunc = hashString.encode()
-h.update(hashFunc)
-xres = h.hexdigest()
+   hashString = str(rand)+k_a
+   hashFunc = hashString.encode()
+   h.update(hashFunc)
+   xres = h.hexdigest()
 
-h2.update(hashFunc)
-ck_a = h2.hexdigest()
+   h2.update(hashFunc)
+   ck_a = h2.hexdigest()
 
-serverSocket.sendto(str(rand).encode(), clientAddress)
+   serverSocket.sendto(str(rand).encode(), clientAddress)
 
-chalAns, clientAddress = serverSocket.recvfrom(2048)
-res = chalAns.decode()
+   chalAns, clientAddress = serverSocket.recvfrom(2048)
+   res = chalAns.decode()
 
-print(res)
-print(xres)
+   print(res)
+   print(xres)
 
-if res == xres: 
-    print("SUCCESS")
-    randCookie = random.randint(1,10)
-    authSuccess = str(randCookie)+str(serverPort)
-    print(authSuccess)
+   if res == xres: 
+      print("SUCCESS")
+      randCookie = random.randint(1,10)
+      authSuccess = str(randCookie)+str(serverPort)
+      print(authSuccess)
 
-    #encrypt and send auth message
-    cipher_suite = Fernet(k_a.encode())
-    authEnc = cipher_suite.encrypt(authSuccess.encode())
-    serverSocket.sendto(authEnc, clientAddress)
+      #encrypt and send auth message
+      cipher_suite = Fernet(k_a.encode())
+      authEnc = cipher_suite.encrypt(authSuccess.encode())
+      serverSocket.sendto(authEnc, clientAddress)
